43cbcf2ce1da3e8285a452d9d7877292dbebbe6b
QueenieCplusplus <58072875+QueenieCplusplus@users.noreply.github.com>
2019-11-22 18:55:01 +0800

    Add files via upload

---
+++ 1122.c
@@ -0,0 +1,19 @@
+//2019, 11/22, 18:30, by Queenie
+// C++ project
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void funcQ(){
+
+    int iA[] = {1, 2, 3, 4, 5};
+    char sA[] = "QUEEN";
+    // char sA[6] = {'Q', 'U', 'E', 'E', 'N', '\0'};
+
+}
+
+int main(){
+    funcQ();
+    return 0;
+}
